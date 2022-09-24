6c3b88a4a462d248ba8ed0afab9f9e51c2fa62a9
Ron Sandstrom <Ron.Sandstrom@rlsand.com>
2022-09-24 16:55:36 -0400

    initial

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 # This is a sample Python script.
+from threestep.result.c_result import Result
+from threestep.std_logging.logs import init_logs, logger
+
+from database_tables.automation_database import AutomationDatabaseTables
+from database_tables.c_staff_master_list_table import StaffMasterList
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main() -> Result:
+    db: AutomationDatabaseTables = AutomationDatabaseTables.Instance()
+
+    p_result = db.get_not_process_staff()
+    show_result(p_result)
+
+    ach_result = db.get_ach_error_items()
+    show_result(ach_result)
+
+    w9_result = db.get_w9_error_items()
+    show_result(w9_result)
+
+    return Result(0)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    tracker = init_logs('staff_database_update_test', console=True, the_version='2.0.0', log_to_file=False,
+                        log_level='info', project_name='webhooks', vm_name='App Engine', job_type='WebHook')
+    tracker.start()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    result = main()
+
+    tracker.end(result.message)
+
